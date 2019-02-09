27815e20d7a0a4dac628a2e19c7fe5379322005f
Jon Ball <jrball3@verizon.net>
2019-02-09 00:59:06 -0500

    update default container name for repo name

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
 docker-compose build && docker-compose up -d --force-recreate
-docker exec -e WAIT_HOSTS=localhost:3000 -it app_user_api_1 sh -c "/wait && npm test"
-docker logs app_user_api_1
+docker exec -e WAIT_HOSTS=localhost:3000 -it nodejs-mongodb-restify-user-api_user_api_1 sh -c "/wait && npm test"
+docker logs nodejs-mongodb-restify-user-api_user_api_1
 docker-compose down
